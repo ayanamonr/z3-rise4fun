750bba725e94844b335d20674a87930232c0d408
Ayana Monroe <ayanam@live.unc.edu>
2022-05-18 12:16:50 -0400

    added z3 v 4.8.17

---
+++ out/z3new.js
@@ -0,0 +1,22 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.init = void 0;
+// @ts-ignore no-implicit-any
+const initModule = require("./z3-built.js");
+// @ts-ignore no-implicit-any
+const wrapper_1 = require("./wrapper");
+__exportStar(require("./wrapper"), exports);
+function init() {
+    return (0, wrapper_1.init)(initModule);
+}
+exports.init = init;
